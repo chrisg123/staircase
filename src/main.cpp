eb4c5cba414f1a11ac94483966d8ed6acdaeb55e
chrisg123 <chrisg_123@hotmail.com>
2023-09-15 17:55:05 -0400

    Read embedded STEP file using opencascade

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
+#include "EmbeddedStepFile.h"
 #include <emscripten.h>
 #include <iostream>
+#include <opencascade/STEPCAFControl_Reader.hxx>
 #include <opencascade/Standard_Version.hxx>
-#include "EmbeddedStepFile.h"
+#include <opencascade/TDocStd_Document.hxx>
+#include <opencascade/XCAFApp_Application.hxx>
 
 int main() {
   std::string occt_version_str =
@@ -15,5 +18,28 @@ int main() {
   EM_ASM({ document.getElementById('stepText').innerHTML = UTF8ToString($0); },
          embeddedStepFile.c_str());
 
+  Handle(XCAFApp_Application) anApp = XCAFApp_Application::GetApplication();
+  Handle(TDocStd_Document) aDoc;
+
+  anApp->NewDocument("MDTV-XCAF", aDoc);
+
+  STEPCAFControl_Reader aStepReader;
+
+  std::istringstream aStepStream(embeddedStepFile);
+
+  IFSelect_ReturnStatus aStatus =
+      aStepReader.ReadStream("Embedded STEP Data", aStepStream);
+
+  if (aStatus == IFSelect_RetDone) {
+    if (!aStepReader.Transfer(aDoc)) {
+      std::cerr << "Error transferring data from STEP to XCAF." << std::endl;
+      return 1;
+    }
+
+    std::cout << "Embedded STEP Data successfully read." << std::endl;
+  } else {
+    std::cerr << "Error reading STEP file." << std::endl;
+    return 1;
+  }
   return 0;
 }
