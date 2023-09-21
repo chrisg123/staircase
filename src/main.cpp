c4fd2fb63d6631f1244da1f5afc61d458c21f4b5
chrisg123 <chrisg_123@hotmail.com>
2023-09-21 08:04:55 -0400

    Move time consuming work to background thread

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,10 @@
 #include <opencascade/TDataStd_Name.hxx>
 #include <opencascade/TDocStd_Document.hxx>
 #include <opencascade/XCAFApp_Application.hxx>
+#include <optional>
+#include <thread>
+
+using DocHandle = Handle(TDocStd_Document);
 
 class Timer {
 public:
@@ -28,13 +32,96 @@ private:
   std::chrono::time_point<std::chrono::high_resolution_clock> start;
 };
 
+bool arePthreadsEnabled();
+void printLabels(TDF_Label const &label, int level = 0);
+std::optional<DocHandle> readInto(std::function<DocHandle()> aNewDoc,
+                   std::istream &fromStream);
+
+void main_loop() {}
+
+int main() {
+  std::string occt_version_str =
+      "OCCT Version: " + std::string(OCC_VERSION_COMPLETE);
+
+  std::cout << occt_version_str << std::endl;
+
+  EM_ASM({ document.getElementById('version').innerHTML = UTF8ToString($0); },
+         occt_version_str.c_str());
+  EM_ASM({ document.getElementById('stepText').innerHTML = UTF8ToString($0); },
+         embeddedStepFile.c_str());
+
+  Handle(XCAFApp_Application) anApp = XCAFApp_Application::GetApplication();
+
+  auto aNewDoc = [&]() -> DocHandle {
+    Handle(TDocStd_Document) aDoc;
+    anApp->NewDocument("MDTV-XCAF", aDoc);
+    return aDoc;
+  };
+
+  std::istringstream fromStream(embeddedStepFile);
+
+  if (arePthreadsEnabled()) {
+    std::cout << "Pthreads enabled" << std::endl;
+    std::thread([&]() {
+      std::optional<DocHandle> docOpt;
+      {
+        Timer timer = Timer("readInto(aNewDoc, fromStream)");
+        docOpt = readInto(aNewDoc, fromStream);
+      }
+      if (docOpt.has_value()) { printLabels(docOpt.value()->Main()); }
+
+      emscripten_cancel_main_loop();
+    }).detach();
+
+    emscripten_set_main_loop(main_loop, 0, 1);
+
+  } else {
+    std::cout << "Pthreads not enabled. Browser may freeze." << std::endl;
+    readInto(aNewDoc, fromStream);
+  }
+
+  return 0;
+}
+
+std::optional<DocHandle> readInto(std::function<DocHandle()> aNewDoc,
+                                  std::istream &fromStream) {
+
+  DocHandle aDoc = aNewDoc();
+  STEPCAFControl_Reader aStepReader;
+
+  IFSelect_ReturnStatus aStatus =
+      aStepReader.ReadStream("Embedded STEP Data", fromStream);
+
+  if (aStatus != IFSelect_RetDone) {
+    std::cerr << "Error reading STEP file." << std::endl;
+    return std::nullopt;
+  }
+
+  bool success = aStepReader.Transfer(aDoc);
+
+  if (!success) {
+    std::cerr << "Transfer failed." << std::endl;
+    return std::nullopt;
+  }
+
+  return aDoc;
+}
+
+bool arePthreadsEnabled() {
+#ifdef __EMSCRIPTEN_PTHREADS__
+  return true;
+#else
+  return false;
+#endif
+}
+
 /**
  * Recursively prints the hierarchy of labels from a TDF_Label tree.
  *
  * @param label The root label to start the traversal from.
  * @param level Indentation level for better readability (default is 0).
  */
-void printLabels(TDF_Label const &label, int level = 0) {
+void printLabels(TDF_Label const &label, int level) {
   for (int i = 0; i < level; ++i) {
     std::cout << "  ";
   }
@@ -51,45 +138,4 @@ void printLabels(TDF_Label const &label, int level = 0) {
   for (TDF_ChildIterator it(label); it.More(); it.Next()) {
     printLabels(it.Value(), level + 1);
   }
-}
-
-int main() {
-  std::string occt_version_str =
-      "OCCT Version: " + std::string(OCC_VERSION_COMPLETE);
-
-  std::cout << occt_version_str << std::endl;
-
-  EM_ASM({ document.getElementById('version').innerHTML = UTF8ToString($0); },
-         occt_version_str.c_str());
-
-  EM_ASM({ document.getElementById('stepText').innerHTML = UTF8ToString($0); },
-         embeddedStepFile.c_str());
-
-  Handle(XCAFApp_Application) anApp = XCAFApp_Application::GetApplication();
-  Handle(TDocStd_Document) aDoc;
-
-  anApp->NewDocument("MDTV-XCAF", aDoc);
-
-  STEPCAFControl_Reader aStepReader;
-
-  std::istringstream aStepStream(embeddedStepFile);
-
-  IFSelect_ReturnStatus aStatus =
-      aStepReader.ReadStream("Embedded STEP Data", aStepStream);
-
-  if (aStatus == IFSelect_RetDone) {
-    if (!aStepReader.Transfer(aDoc)) {
-      std::cerr << "Error transferring data from STEP to XCAF." << std::endl;
-      return 1;
-    }
-
-    std::cout << "Embedded STEP Data successfully read." << std::endl;
-    std::cout << "Document structure:" << std::endl;
-    printLabels(aDoc->Main());
-
-  } else {
-    std::cerr << "Error reading STEP file." << std::endl;
-    return 1;
-  }
-  return 0;
 }
