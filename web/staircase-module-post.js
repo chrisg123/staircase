f578ac46e719082db479f6bee495702924009f78
chrisg123 <chrisg_123@hotmail.com>
2024-01-25 18:45:38 -0500

    Cleanup demo index.html; Accommodate multiple step viewers;

--- web/staircase-module-post.js
+++ web/staircase-module-post.js
@@ -1,5 +1,3 @@
-var Staircase = typeof Staircase != "undefined" ? Staircase : {};
-
 if (typeof document !== "undefined") {
     const moduleArg = {
         locateFile: (file, scriptDirectory) => {
@@ -17,10 +15,36 @@ if (typeof document !== "undefined") {
     };
 
     createStaircaseModule(moduleArg).then(function (module) {
-        let viewer = new module.StaircaseViewer("staircase-container");
-        Staircase.viewer = viewer;
-        if (typeof Staircase.onViewerReady === "function") {
-            Staircase.onViewerReady(viewer);
+        if (!Staircase) {
+            console.error("Staircase object not defined.");
+            return;
+        }
+        if (!Staircase.containerIds) {
+            Staircase.containerIds = [];
+        }
+
+        if (Staircase.containerId) {
+            Staircase.containerIds.push(Staircase.containerId)
+        }
+        if (Staircase.containerIds.length === 0) {
+            console.error("No container ids defined on Staircase object.")
+            return;
+        }
+        if (typeof Staircase.onViewerReady !== "function") {
+            console.error("Staircase.onViewerReady function not defined on Staircase object.")
+            return;
+        }
+
+        if (typeof Staircase.viewers === "undefined") {
+            Staircase.viewers = {};
+        }
+
+        for (let containerId of Staircase.containerIds) {
+            let viewer = new module.StaircaseViewer(containerId);
+            Staircase.viewers[containerId] = viewer;
+            if (typeof Staircase.onViewerReady === "function") {
+                Staircase.onViewerReady(viewer);
+            }
         }
     });
 }
