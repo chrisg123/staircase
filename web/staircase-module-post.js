4618b504d4de195eb8a3c3d0385017c3c757d133
chrisg123 <chrisg_123@hotmail.com>
2024-01-26 17:21:35 -0500

    Enhance Staircase Library for Flexible Loading Order

--- web/staircase-module-post.js
+++ web/staircase-module-post.js
@@ -1,4 +1,8 @@
-if (typeof document !== "undefined") {
+if (typeof document !== "undefined") { // To avoid this code block in worker threads
+    window.Staircase =
+        typeof window.Staircase !== "undefined" ? window.Staircase : {};
+    window.Staircase._onViewerReady = null;
+
     const moduleArg = {
         locateFile: (file, scriptDirectory) => {
             const base = scriptDirectory.endsWith("/")
@@ -15,36 +19,58 @@ if (typeof document !== "undefined") {
     };
 
     createStaircaseModule(moduleArg).then(function (module) {
-        if (!Staircase) {
-            console.error("Staircase object not defined.");
-            return;
-        }
-        if (!Staircase.containerIds) {
-            Staircase.containerIds = [];
-        }
-
-        if (Staircase.containerId) {
-            Staircase.containerIds.push(Staircase.containerId)
-        }
-        if (Staircase.containerIds.length === 0) {
-            console.error("No container ids defined on Staircase object.")
-            return;
-        }
-        if (typeof Staircase.onViewerReady !== "function") {
-            console.error("Staircase.onViewerReady function not defined on Staircase object.")
-            return;
-        }
-
-        if (typeof Staircase.viewers === "undefined") {
-            Staircase.viewers = {};
-        }
-
-        for (let containerId of Staircase.containerIds) {
-            let viewer = new module.StaircaseViewer(containerId);
-            Staircase.viewers[containerId] = viewer;
-            if (typeof Staircase.onViewerReady === "function") {
-                Staircase.onViewerReady(viewer);
+        window.Staircase.initializeViewers = function () {
+            if (!window.Staircase.containerIds) {
+                window.Staircase.containerIds = [];
             }
+
+            if (window.Staircase.containerId) {
+                window.Staircase.containerIds.push(
+                    window.Staircase.containerId,
+                );
+            }
+
+            if (Staircase.containerIds.length === 0) {
+                console.warn(
+                    "Staircase.initializeViewers function called but no" +
+                        " container ids defined on Staircase object.",
+                );
+                return;
+            }
+
+            if (typeof window.Staircase.viewers === "undefined") {
+                window.Staircase.viewers = {};
+            }
+
+            if (typeof Staircase.onViewerReady !== "function") {
+                console.warn(
+                    "Staircase.onViewerReady function is not defined." +
+                        " You probably want to define this to capture" +
+                        " a reference to a viewer to load STEP files",
+                );
+            }
+
+            for (let containerId of window.Staircase.containerIds) {
+                let viewer = new module.StaircaseViewer(containerId);
+                window.Staircase.viewers[containerId] = viewer;
+                if (typeof window.Staircase.onViewerReady === "function") {
+                    window.Staircase.onViewerReady(viewer);
+                }
+            }
+        };
+
+        if (typeof window.Staircase.onViewerReady !== "undefined") {
+            window.Staircase.initializeViewers();
+        } else {
+            Object.defineProperty(window.Staircase, "onViewerReady", {
+                set: function (callback) {
+                    this._onViewerReady = callback;
+                    window.Staircase.initializeViewers();
+                },
+                get: function () {
+                    return this._onViewerReady;
+                },
+            });
         }
     });
 }
